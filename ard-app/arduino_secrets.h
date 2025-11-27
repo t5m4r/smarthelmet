7d9ece6a80f8ba13c3f63d6a4d9056053d940736
Siddharth Mathur <siddharth@getfastah.com>
2025-11-27 20:41:26 +0530

    remove unwanted code

--- ard-app/arduino_secrets.h
+++ ard-app/arduino_secrets.h
@@ -1,8 +1,4 @@
 ///////please enter your sensitive data/Wifi passwords etc in this file
 char ssid[] = "CALVIN";  // your network SSID (name)
 char pass[] = "DEADCOFFEE1";  // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;           // your network key index number (needed only for WEP)
 
-// API key for Fastah IP detection
-const char fastah_secret_key[] = "";
-const char kFastahHttpHeader[] = "Fastah-Key";
